f1320f18aaa03d4ef31049e82c22777b50320198
BoDee Angus <bodee.angus@gmail.com>
2024-03-04 15:34:15 -0700

    Need to work on this on a different computer

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
 import axios from 'axios';
 
-export default async function (method, url, data = null) {
+export default async function (url, method, { headers, params, data } = {}) {
   try {
-    let dataToReturn;
     switch (method) {
-      case 'get':
-        dataToReturn = !data
-          ? await axios.get(url)
-          : await axios.get(url, data);
-        break;
-      case 'post':
-        dataToReturn = !data
-          ? await axios.post(url)
-          : await axios.post(url, data);
-        break;
-      case 'put':
-        dataToReturn = !data
-          ? await axios.put(url)
-          : await axios.put(url, data);
-        break;
-      case 'delete':
-        dataToReturn = !data
-          ? await axios.delete(url)
-          : await axios.delete(url, data);
-        break;
+      case 'GET':
+        return await axios.get(url, { headers, params });
+      case 'POST':
+        return await axios.post(url, data, { headers });
+      case 'PUT':
+        return await axios.put(url, data, { headers });
+      case 'PATCH':
+        return await axios.patch(url, data, { headers });
+      case 'DELETE':
+        return await axios.delete(url, { headers, params });
       default:
         throw new Error('Invalid method');
     }
-    console.log('Axios_Fetcher successfully fetched data: ', dataToReturn.data);
-    return dataToReturn.data;
   } catch (error) {
     console.error('Axios_Fetcher encountered an error: ', error);
     throw error;
